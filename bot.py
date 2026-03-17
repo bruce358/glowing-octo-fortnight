7b20f904ab09dc4dc6472710e0a777f6f434c651
bruce358 <paitynbruce561@gmail.com>
2026-03-17 15:14:51 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ import time
 import random
 import os
 
-repo = "solzen77/polymarket-sports-copy-trading-bot"
-LIMIT = 30
+repo = "tinyhumansai/neocortex"
+LIMIT = 20
 
 # get tokens from GitHub Secrets
 tokens = os.getenv("TOKENS")
@@ -27,23 +27,7 @@ for token in tokens:
     print("Processing account...")
 
     try:
-        # -------- STAR CHECK --------
-        star_check = requests.get(
-            f"https://api.github.com/user/starred/{repo}",
-            headers=headers,
-            timeout=10
-        )
-
-        if star_check.status_code == 204:
-            print("Already starred")
-        else:
-            requests.put(
-                f"https://api.github.com/user/starred/{repo}",
-                headers=headers,
-                timeout=10
-            )
-            print("Starred")
-
+        
         # -------- WATCH CHECK --------
         watch_check = requests.get(
             f"https://api.github.com/repos/{repo}/subscription",
@@ -62,17 +46,7 @@ for token in tokens:
             )
             print("Watching")
 
-        # -------- FORK --------
-        fork = requests.post(
-            f"https://api.github.com/repos/{repo}/forks",
-            headers=headers,
-            timeout=10
-        )
-
-        if fork.status_code == 202:
-            print("Forked")
-        else:
-            print(f"Fork failed / already forked ({fork.status_code})")
+        
 
     except Exception as e:
         print("Error:", e)
